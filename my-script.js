628e08af87b33d6fba1f90a33870f2018385ca84
Yatendra kumar sharma <yatendrakumarskd@gmail.com>
2023-02-02 01:39:27 +0530

    Added files with conflict issue replicated

---
+++ my-script.js
@@ -0,0 +1,9 @@
+jQuery.noConflict();
+(function($){
+			
+			// datetimepicker function
+            $(function () {
+                $('#datetimepicker1').datetimepicker({ sideBySide: true, debug: true });
+            });
+			
+})(jQuery);
